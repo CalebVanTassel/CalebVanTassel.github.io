fa51f4c9be773b75c8d493f486f42d8745f695fe
calebvantassel <calebvantassel@gmail.com>
2018-12-06 18:11:55 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+// Check if element is in viewport
+// Source: https://medium.com/talk-like/detecting-if-an-element-is-in-the-viewport-jquery-a6a4405a3ea2
+$.fn.isInViewport = function() {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(document).ready(function() {
+
+    // Show elements in viewport
+    $(".block").each(function() {
+        if ($(this).isInViewport())
+            $(this).css("opacity", "1");
+    });
+     
+    // Fade in elements
+    // Source: https://codepen.io/annalarson/pen/GesqK 
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.block').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ) {
+                $(this).animate({'opacity':'1'}, 5); 
+            } 
+        });  
+    }); 
+
+
+    $("#menu").click(function() {
+        $(".hamburger:first").toggleClass("is-active");
+        $("#sideBar").toggleClass("sideBarActive");
+    });
+
+});
+
